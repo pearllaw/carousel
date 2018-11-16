a5bc19cc499a97c78bdac99292910393ffdb53c0
Pearl Law <pearllynnelaw@gmail.com>
2018-11-16 09:25:22 -0800

    Create a Carousel class component in app.js that takes a dogs array in appState #1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 export default class Carousel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dogs: [
+        {breed: 'Corgi', description: 'Origin: Wales', imageUrl: 'https://d3qvqlc701gzhm.cloudfront.net/thumbs/4ffc0db4e2fdb1c2bbb21218e1f77a78e154961df90dc485de5e7121fd99e431-750.jpg'},
+        {breed: 'German Shepherd', description: 'Origin: Germany', imageUrl: 'http://texasworkingshepherds.com/wp-content/uploads/2013/09/German-SHepherd.png'},
+        {breed: 'Golden Retriever', description: 'Origin: Scotland', imageUrl: 'http://images6.fanpop.com/image/photos/41400000/cute-golden-retrievers-eating-ice-cream-golden-retreivers-41435145-500-500.jpg'}
+      ]
+    }
+  }
   render() {
     return(
 
